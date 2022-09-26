78aed0facc4d55eb60408db54ac2a96138d8f7e2
lzy-Jerry <763626908@qq.com>
2022-09-26 10:27:14 +0800

    feat: webpack配置学习

---
+++ 01_webpack初体验/src/index.js
@@ -0,0 +1,5 @@
+import { sum } from "./js/math.js";
+const { mul } = require('./js/math.js')
+
+console.log(sum(1, 2))
+console.log(mul(1, 2))
---
+++ 02_webpack配置之CSS/src/js/index.js
@@ -0,0 +1,5 @@
+import { sum } from "./math.js";
+const { mul } = require('./math.js')
+
+console.log(sum(1, 2))
+console.log(mul(1, 2))
