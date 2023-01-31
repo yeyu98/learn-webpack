6ab7763a8fd9c20bf708c579ff733223f7fc093c
lzy-Jerry <763626908@qq.com>
2023-01-31 20:44:07 +0800

    feat: esm

--- 06_webpack模块化原理/src/module/commonjs.js
+++ 06_webpack模块化原理/src/module/commonjs.js
@@ -2,7 +2,7 @@
  * @Author: lzy-Jerry
  * @Date: 2023-01-30 21:05:09
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-01-30 21:08:43
+ * @LastEditTime: 2023-01-30 21:58:31
  * @FilePath: \learn-webpack\06_webpack模块化原理\src\module\commonjs.js
  * @Description: 
  */
@@ -37,7 +37,7 @@ function __webpack_require__(moduleId) {
     return __webpack_module_cache__[moduleId].exports;
   }
 
-  // NOTE 这里为什么需要加上括号？
+  // TODO 这里为什么需要加上括号？
   // 等同于 var module = __webpack_module_cache__[moduleId] = {exports: {}};
   // 初始化module以及缓存module对象为 {exports:{}}
   var module = (__webpack_module_cache__[moduleId] = {
@@ -47,10 +47,11 @@ function __webpack_require__(moduleId) {
   // 将初始化之后的module={exports: {}} 传入当前模块函数中
   __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 
+  // module.exorts = {dateFormat, priceFormat}
   return module.exports;
 }
 
-// NOTE 这里的！是将后面的内容作为一个表达式执行 此处涉及知识盲区
+// TODO 这里的！是将后面的内容作为一个表达式执行 此处涉及知识盲区
 // 将函数前面使用()、!、+、-、~等符号都能将函数变成函数表达式，解析器就能识别并执行
 // 此处等同于立即执行函数
 !(function () {
--- 06_webpack模块化原理/webpack.config.js
+++ 06_webpack模块化原理/webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: lzy-Jerry
  * @Date: 2022-09-29 19:18:35
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-01-30 20:18:29
+ * @LastEditTime: 2023-01-30 21:14:09
  * @FilePath: \learn-webpack\06_webpack模块化原理\webpack.config.js
  * @Description: 
  */
@@ -13,7 +13,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development', // mode为development时devtool 默认的就是eval-source-map
     devtool: 'source-map',
-    entry: './src/common_index.js',
+    entry: './src/esmodule_index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './build')
