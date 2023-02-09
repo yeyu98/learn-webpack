0f7b90c8c77702eafea6c6d94392b15d220b4092
lzy-Jerry <763626908@qq.com>
2023-02-09 21:07:46 +0800

    feat: eval、source-map、eval-source-map

--- 07_webpack的source-map/webpack.config.js
+++ 07_webpack的source-map/webpack.config.js
@@ -2,8 +2,8 @@
  * @Author: lzy-Jerry
  * @Date: 2022-09-29 19:18:35
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-02-09 20:13:15
- * @FilePath: \learn-webpack\06_webpack模块化原理\webpack.config.js
+ * @LastEditTime: 2023-02-09 21:00:04
+ * @FilePath: \learn-webpack\07_webpack的source-map\webpack.config.js
  * @Description: 
  */
 const path = require('path')
@@ -12,7 +12,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development', // mode为development时devtool 默认的就是eval-source-map
-    devtool: 'eval',
+    devtool: 'eval-source-map',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
