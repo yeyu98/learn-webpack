44931ad6a8cd2828231e2ccab4f08f1f09ac7b15
lzy-Jerry <763626908@qq.com>
2023-03-09 20:54:34 +0800

    feat:test1

--- 08_webpack之babel/src/index.js
+++ 08_webpack之babel/src/index.js
@@ -2,7 +2,7 @@
  * @Author: lzy-Jerry
  * @Date: 2022-09-29 19:18:35
  * @LastEditors: lzy-Jerry
- * @LastEditTime: 2023-03-07 21:34:11
+ * @LastEditTime: 2023-03-09 20:45:42
  * @FilePath: \learn-webpack\06_webpack模块化原理\src\index.js
  * @Description: 
  */
@@ -12,7 +12,7 @@ const message = "Hello, babel"
 const foo = (info) => {
     console.log(info)
 }
-
+console.log("test")
 foo(message)
 
 class Babel {
